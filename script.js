15f2c0f9f37cba17834151361ca25d34ef986a33
Dov <dovindustries@gmail.com>
2020-10-24 21:22:31 -0400

    1. Completed index.html boilderplate code and added script.js. 2. Started commenting instructions on script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. what is the first child of the table?
+
+// 2a. what is the first child of a row in a table?
+    // 2b. what's the next sibling of a row in a table?
+
+// 3. what is the next sibling of a data element in the table?
+
+// need a method "next child" to traverse from child to child
+// Array.from(nodelist) converts a node list into an array.
