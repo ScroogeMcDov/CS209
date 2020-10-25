e42ada8662870084042834a144b26de17f670924
Dov <dovindustries@gmail.com>
2020-10-24 22:15:38 -0400

    1. Completed index.html boilderplate code and added script.js. 2. Started commenting instructions on script.js file 3. Added skeleton code (of essential methods) to script.js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@
 
 // need a method "next child" to traverse from child to child
 // Array.from(nodelist) converts a node list into an array.
+
+function makeTable(rows, cols) {}
+function makeButtons() {}
+function addButtonFunctions() {} // add functions that the buttons will perform (using event listeners)
+function addStyles() {}
+function createAndAppend() {newElement, parent} // append newElement to parent.
+function createAndAppendwText() {newElement, parent, text} // takes an Element and appends it to parent and gives it the appropriate text
+
